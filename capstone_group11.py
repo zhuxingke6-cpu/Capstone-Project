224905b40780e099937b73cba4b945bac88c3f56
zhuxingke6-cpu <zhuxingke6@gmail.com>
2026-02-02 22:25:56 -0500

    url

--- capstone_group11.py
+++ capstone_group11.py
@@ -1,1 +1,7 @@
 import import pandas as pd
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("mahatiratusher/flight-price-dataset-of-bangladesh")
+
+print("Path to dataset files:", path)
